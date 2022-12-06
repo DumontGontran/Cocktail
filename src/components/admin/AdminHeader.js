0bbd82edc725ddd4685601a7631a1f2796dc76cf
DumontGontran <dumont.gontran@orange.fr>
2022-12-06 19:38:46 +0100

    update AdminHeader + styles

--- src/components/admin/AdminHeader.js
+++ src/components/admin/AdminHeader.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { accountService } from '../../_services/account.service';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 const AdminHeader = () => {
+    let navigate = useNavigate();
+    const [over, setOver] = useState(false);
+
+    const logout =() => {
+        accountService.logout();
+        navigate('/');
+    };
+
     return (
         <div className='AdminHeader'>
-            <h1>Administration Panel</h1>
+            <h1>Panneau d'administration</h1>
+            <button onClick={logout} onMouseOver={() => setOver(true)}
+        onMouseLeave={() => setOver(false)}><FontAwesomeIcon icon={faRightFromBracket} size= 'lg' style={over ? { color: 'darkcyan' } : {}} /></button>
         </div>
     )
 }
