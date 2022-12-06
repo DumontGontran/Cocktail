e4d444f966e9ba006f262b33153dfae2afcb599c
DumontGontran <dumont.gontran@orange.fr>
2022-12-06 19:37:58 +0100

    update AuthGuard

--- src/_helpers/AuthGuard.js
+++ src/_helpers/AuthGuard.js
@@ -1,9 +1,8 @@
 import { Navigate } from "react-router-dom";
+import { accountService } from "../_services/account.service";
 
 const AuthGuard = ({children}) => {
-    let logged = false;
-
-    if(!logged) {
+    if(!accountService.isLogged()) {
         return <Navigate to='/auth/login' />
     }
 
