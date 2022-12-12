477426686b4aa0c88c865facb19b2685cc4f1002
DumontGontran <dumont.gontran@orange.fr>
2022-12-12 20:12:26 +0100

    import public style for services in publicLayout

--- src/pages/public/PublicLayout.js
+++ src/pages/public/PublicLayout.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/public/Header';
 
+import '../../pages/public/public.css';
+
 const PublicLayout = () => {
     return (
         <div>
