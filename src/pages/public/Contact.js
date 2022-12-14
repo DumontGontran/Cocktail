c0735a0b8d29c212c5fd0e668a16b28f237cc552
DumontGontran <dumont.gontran@orange.fr>
2022-12-14 19:48:06 +0100

    contact informations available

--- src/pages/public/Contact.js
+++ src/pages/public/Contact.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            Contact Works !
+        <div className='Contact'>
+            <h1>Contact</h1>
+            <div>
+                <h2>Dumont Gontran</h2>
+                <h3>Développeur Web</h3>
+                <p>Email: <span>dumont.gontran@orange.fr</span></p>
+                <p>Téléphone: <span>06 47 22 56 37</span></p>
+                <p>Portfolio : <a href='https://server-gontran-dumont.fr'>Cliquez-ici</a></p>
+            </div>
         </div>
     );
 };
