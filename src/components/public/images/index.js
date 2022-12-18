bcc9f330df3157d2bdde7f94e55933fb9955e5cb
DumontGontran <dumont.gontran@orange.fr>
2022-12-18 11:48:29 +0100

    exports config of all images

---
+++ src/components/public/images/index.js
@@ -0,0 +1,6 @@
+export { default as cocktail0 } from './0.jpg';
+export { default as cocktail1 } from './1.jpg';
+export { default as cocktail2 } from './2.jpg';
+export { default as cocktail3 } from './3.jpg';
+export { default as cocktail4 } from './4.jpg';
+export { default as cocktail5 } from './5.jpg';
